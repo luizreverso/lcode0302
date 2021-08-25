b567d7be3a70cd7d9baa5dea6c9319d7537cd2e9
Luiz Silva <luizreverso@gmail.com>
2021-08-24 21:06:55 -0300

    yarn and babel

---
+++ api-example/app.js
@@ -0,0 +1,6 @@
+const app = {
+    teste: "blblblba"
+}
+console.log("cheguei aqui no app")
+
+export default app;
--- desafios/ecommerce-ebook/ecommerce.js
+++ desafios/ecommerce-ebook/ecommerce.js
@@ -1,4 +1,5 @@
 const User = require("./user")
+// import User from "./user"
 
 class Ecommerce {
     registeredUsers = []
