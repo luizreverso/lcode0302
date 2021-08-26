74c31e484e9d1117d31fc7cb9e54054d3cfb1041
Ana Higo <anahigo.arq@gmail.com>
2021-08-26 20:33:05 -0300

    alteraçoes

---
+++ crud-example/Ana_Higo/ana_higo.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const dogs = [];
+
+app.get("/", (_req, res) => {
+  res.send(dogs);
+});
+
+app.get("/dogs/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.send(dogs[id]);
+});
+
+app.post("/dogs", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.end("Informe um nome");
+  }
+  dogs.push(name);
+
+  console.log(dogs);
+
+  res.send("Adicionado com sucesso");
+});
+
+app.put("/dogs/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  dogs[id] = name;
+
+  res.send(dogs[id]);
+});
+
+app.delete("/dogs/:id", (req, res) => {
+  const { id } = req.params;
+
+  dogs.splice(id, 1);
+
+  res.send(dogs);
+});
+
+// seguindo o padrao CRUD (create, read, update, delete), criar os endpoints restantes
+// bonus round!!!: criar um endpoint de com a intencao de trazer um unico cachorro especificado por algum parametro
+
+// app.get("/teste", (req, res) => {
+//     console.log("ola eu estou no servidor! :)");
+
+//     res.send("Hello from teste")w
+// })
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
---
+++ desafios/ecommerce-ebook/Ana_Higo/user.js
@@ -0,0 +1,13 @@
+class User {
+  email = "";
+  password = "";
+  selectedItems = [];
+   = {};
+
+  constructor(user) {
+    this.email = user.email;
+    this.valor = user.senha;
+    this.itensSelecionado = user.
+
+  }
+}
