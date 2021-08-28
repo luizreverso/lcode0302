3821634a9789465c0491a1d7f42420ab9f1ec04f
Maria Baeta <m.martinsdg@gmail.com>
2021-08-27 21:53:35 -0300

    feat: :rocket: Requisição a numeros aleatorios a 2 api

---
+++ desafios/divideFetch/consumer/index2.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch')
+
+const divide = (num1, num2) => {
+  if (num2 !== 0) {
+    const result = num1 / num2;
+    console.log(result);
+    return;
+  }
+  console.log("Não é possivel fazer um divisão por 0");
+};
+
+const numberX = async () => {
+  const response = await fetch('http://localhost:6000/getx');
+  const data = response.json();
+  return data;
+};
+
+const numberY = async () => {
+  const response = await fetch('http://localhost:6000/gety');
+  const data = response.json();
+  return data;
+};
+
+
+const getNumbers = async () => {
+  const { x } = await numberX();
+  const { y } = await numberY();
+  divide(x, y)
+}
+
+getNumbers()
+
