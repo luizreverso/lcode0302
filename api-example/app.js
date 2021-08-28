5500d5c46607f35b81b13de788b66aaab4556476
Kássia Oliveira <kassiafo@gmail.com>
2021-08-28 13:31:52 -0300

    Delete api-example directory

--- api-example/app.js
+++
@@ -1,6 +0,0 @@
-const app = {
-    teste: "blblblba"
-}
-console.log("cheguei aqui no app")
-
-export default app;
--- api-example/index.js
+++
@@ -1,4 +0,0 @@
-import App from "./app.js" // ecma
-// const App = require("./app") // common js
-
-console.log("Hello LuizCode")
