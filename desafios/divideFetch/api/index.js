5df08e27c5ad8b3cc86f27cfc56bcb5d2afa3dbe
Kássia Oliveira <kassiafo@gmail.com>
2021-08-28 13:21:48 -0300

    Subindo desafio consumir API e realizar uma operação de divisão.

--- desafios/divideFetch/api/index.js
+++ desafios/divideFetch/api/index.js
@@ -13,4 +13,20 @@ app.get("/numbers", (_req, res) => {
     })
 })
 
+app.get("/getx", (_req, res) => {
+    const x = Math.floor(Math.random() * 10)
+
+    res.send({ x: x !== 7 ? x : undefined })
+})
+
+app.get("/gety", (_req, res) => {
+    const y = Math.floor(Math.random() * 10)
+
+    if (y === 4) throw "error because y === 4 :)"
+
+    res.send({ y })
+})
+
+
+
 app.listen(6000, () => console.log("servidor iniciado na porta 6000"))
---
+++ desafios/divideFetch/consumer/index.js
@@ -0,0 +1,71 @@
+// escrever um programa que consome a api, recebe os valores e calcula x/y e retorna o resultado :)
+// GET http://localhost:6000/numbers
+
+const fetch = require("node-fetch");
+
+function dividir(x, y) {
+    // Um throw sempre vai cair no catch
+    if(x == undefined){
+        throw "X é undefined."
+    }else if (y === 0){
+        throw "Impossível dividir por zero!"
+    }
+    else{
+        res = x / y
+        return res
+    }
+}
+
+const divisao = async () => {
+    // console.log("DIVISÃO DE X E Y DE UMA ÚNICA API")
+    const { x, y } = await fetch("http://localhost:6000/numbers")
+        .then((res) => {
+            // console.log("Response do index.js da API: ")
+            // console.log(res)
+            return res.json()
+        })
+    console.log("DIVISÃO DE X E Y DE UMA ÚNICA API")
+    console.log("Valor de X: ", x)
+    console.log("Valor de Y: ", y)
+    try {
+        resultado = dividir(x, y)
+        console.log("O resultado da divisão é: ", resultado)
+    } catch (error) {
+        console.log("Error: ", error)
+    } finally {
+        console.log("Operação concluída!")
+        console.log("------------------------------------------------------------------------------------------------------------------------------------")
+    } 
+}
+
+
+//escrever o mesmo programa, porem x e y vem de duas apis distintas.
+//GET http://localhost:6000/getx GET http://localhost:6000/gety
+
+const getNumbers = async () => {
+    // console.log("DIVISÃO DE X E Y API DISTINTAS")
+    const{x} =  await fetch("http://localhost:6000/getx")
+        .then((res) => {
+            return res.json()
+        })
+    const{y} =  await fetch("http://localhost:6000/gety")
+        .then((res) => {
+            return res.json()
+        })
+    console.log("DIVISÃO DE X E Y API DISTINTAS")
+    console.log("Valor de X: ", x)
+    console.log("Valor de Y: ", y)
+    try {
+        resultado = dividir(x, y)
+        console.log("O resultado da divisão é: ", resultado)
+    } catch (error) {
+        console.log("Error: ", error)
+    } finally {
+        console.log("Operação concluída!")
+        console.log("------------------------------------------------------------------------------------------------------------------------------------")
+    } 
+} 
+
+
+divisao()
+getNumbers()
